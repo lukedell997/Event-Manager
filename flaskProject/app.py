6c94f09ac28bbb75aaa5c28c5ee027ec0d1895ca
Zane Hassan <77768102+staffmage@users.noreply.github.com>
2021-11-30 23:27:17 -0600

    Added the post methods

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, request, flash, session
 from datetime import timedelta
 
-
-
 app = Flask(__name__)
 app.permanent_session_lifetime = timedelta(hours=5)
 
@@ -11,9 +9,9 @@ app.secret_key = "hello"
 admin = False
 
 
-@app.route('/index')
-@app.route('/')
-@app.route('/index.html')
+@app.route('/index' , methods=["POST", "GET"])
+@app.route('/', methods=["POST", "GET"])
+@app.route('/index.html', methods=["POST", "GET"])
 def index():  # put application's code here
     popEventsTitle = []
     popEventsDetails = []
@@ -28,12 +26,13 @@ def index():  # put application's code here
         search = request.form
         # DO A SEARCH IDK HOW YET
 
-    return render_template("index.html", popEventsTitle = popEventsTitle, popEventsDetails = popEventsDetails,
-                           popEventsButton = popEventsButton, nearEventsTitle = nearEventsTitle,
-                           nearEventsDetails = nearEventsDetails, nearEventsButton = nearEventsButton, search=search)
+    return render_template("index.html", popEventsTitle=popEventsTitle, popEventsDetails=popEventsDetails,
+                           popEventsButton=popEventsButton, nearEventsTitle=nearEventsTitle,
+                           nearEventsDetails=nearEventsDetails, nearEventsButton=nearEventsButton, search=search)
 
-@app.route('/registerPage.html')
-@app.route('/registerPage')
+
+@app.route('/registerPage.html', methods=["POST", "GET"])
+@app.route('/registerPage', methods=["POST", "GET"])
 def registerPage():  # put application's code here
     userFirstName = request.form["fn"]
     userLastName = request.form["ln"]
@@ -46,22 +45,24 @@ def registerPage():  # put application's code here
     userNumber = request.form["phone"]
 
     # WE SHOULD ADD A BUTTON TO CREATE THE ACCOUNT then send them to the login page
-    if 'submit' in request.form:
-        # PUSH THE DATA TO THE DATABASE!!!!!!!!!
-        return redirect(url_for("login")) #????
+    if request.method == "POST":
+        if 'submit' in request.form:
+            # PUSH THE DATA TO THE DATABASE!!!!!!!!!
+            return redirect(url_for("login"))  # ????
     return render_template("registerPage.html")
 
 
 @app.route('/loginPage.html', methods=["POST", "GET"])
 @app.route('/loginPage', methods=["POST", "GET"])
 def loginPage():  # put application's code here
-    if 'remeber' in request.form:
-        app.permanent_session_lifetime = timedelta(weeks=10000)
-    elif 'remeber' not in request.form:
-        app.permanent_session_lifetime = timedelta(seconds=0)
+
     if request.method == "POST":
+        if 'remember' in request.form:
+            app.permanent_session_lifetime = timedelta(weeks=10000)
+        elif 'remember' not in request.form:
+            app.permanent_session_lifetime = timedelta(seconds=0)
         session.permanent = True
-        user = request.form["nm"]       # NEED TO CHECK THAT USER EXISTS
+        user = request.form["nm"]  # NEED TO CHECK THAT USER EXISTS
         password = request.form["pw"]
         session["user:"] = user
         return redirect(url_for("user"))
@@ -69,16 +70,14 @@ def loginPage():  # put application's code here
     return render_template("loginPage.html")
 
 
-
-@app.route("/index_userLoggedIn.html")
-@app.route("/index_userLoggedIn")
+@app.route("/index_userLoggedIn.html", methods=["POST", "GET"])
+@app.route("/index_userLoggedIn", methods=["POST", "GET"])
 def user():
-
     if "user" in session:
         search = None
         if request.method == "POST":
             search = request.form
-            #DO A SEARCH IDK HOW YET
+            # DO A SEARCH IDK HOW YET
 
         user = session["user"]
 
@@ -86,31 +85,30 @@ def user():
         myEventTitle = ""
         myEventDetails = ""
 
-        attendingEventsTitle = [] # FILL WITH USERES EVENTS
-        attendingEventsDate= [] # FILL WITH 6 popular EVENTS
-        attendingEventsDeadline = [] # FILL WITH 6 NEAR BY EVENTS
+        attendingEventsTitle = []  # FILL WITH USERES EVENTS
+        attendingEventsDate = []  # FILL WITH 6 popular EVENTS
+        attendingEventsDeadline = []  # FILL WITH 6 NEAR BY EVENTS
 
         popEventTitle = []
         popEventDetails = []
-        if "clicked" in request.form:
-            return # NEED TO FIX THIS BY CHANGING EVENTDEAITLS.HTML TO CONTAIN NAME IN URL
-        return render_template("index_userLoggedIn.html", name = user, myEventImage = myEventImage,
-                               myEventTitle = myEventTitle, myEventDetails = myEventDetails,
-                               attendingEventsTitle = attendingEventsTitle, attendingEventsDate = attendingEventsDate,
-                               attendingEventsDeadline = attendingEventsDeadline, popEventTitle= popEventTitle,
-                               popEventDetails = popEventDetails, search = search)
+        if "clicked" in request.form and request.method == "POST":
+            return  # NEED TO FIX THIS BY CHANGING EVENTDEAITLS.HTML TO CONTAIN NAME IN URL
+        return render_template("index_userLoggedIn.html", name=user, myEventImage=myEventImage,
+                               myEventTitle=myEventTitle, myEventDetails=myEventDetails,
+                               attendingEventsTitle=attendingEventsTitle, attendingEventsDate=attendingEventsDate,
+                               attendingEventsDeadline=attendingEventsDeadline, popEventTitle=popEventTitle,
+                               popEventDetails=popEventDetails, search=search)
 
     else:
-        return redirect(url_for("login")) #????
-
+        return redirect(url_for("login"))  # ????
 
 
 # This is for a logout page that might be made
 @app.route("/logout")
 def logout():
     session.pop("user", None)
-    #flash("You have been logged out!", "info")
-    return  redirect(url_for("login")) #????
+    # flash("You have been logged out!", "info")
+    return redirect(url_for("login"))  # ????
 
 
 @app.route('/add_editEvents.html')
@@ -118,10 +116,11 @@ def logout():
 def add_editEvents():  # put application's code here
     return render_template("add_editEvents.html")
 
-@app.route('/eventDetails.html')
-@app.route('/eventDetails')
-def eventDetails():  # put application's code here
-    #NEED TO PULL ALL THE VARIABLES THAT ARE BEING PASSED FROM THE DATABASE
+
+@app.route('/eventDetails.html', methods=["POST", "GET"])
+@app.route('/eventDetails', methods=["POST", "GET"])
+def eventDetails(eventId):  # put application's code here
+    # NEED TO PULL ALL THE VARIABLES THAT ARE BEING PASSED FROM THE DATABASE
     eventDate = []
     eventTitle = []
     eventPrice = []
@@ -129,50 +128,53 @@ def eventDetails():  # put application's code here
     eventAddress = []
     eventTags = []
     if "user" in session:
-        if "attend" in request.form:
+        if "attend" in request.form and request.method == "POST":
             user = session["user"]
-            #ADD USER TO THE DATABASE LIST OF USERS ATTENDING THE EVENT
-    return render_template("eventDetails.html", eventDate = eventDate, eventTitle = eventTitle, eventPrice = eventPrice,
-                           eventDescription = eventDescription, eventAddress = eventAddress, eventTags = eventTags)
+            # ADD USER TO THE DATABASE LIST OF USERS ATTENDING THE EVENT
+    return render_template("eventDetails.html", eventDate=eventDate, eventTitle=eventTitle, eventPrice=eventPrice,
+                           eventDescription=eventDescription, eventAddress=eventAddress, eventTags=eventTags)
 
-@app.route('/manageEvents.html')
-@app.route('/manageEvents')
+
+@app.route('/manageEvents.html', methods=["POST", "GET"])
+@app.route('/manageEvents', methods=["POST", "GET"])
 def manageEvents():  # put application's code here
     if "user" in session:
         usersEvents = []  # FILL THESE WITH THE APPROPRIATE DATA
         userEventsDates = []
         userEventsTime = []
-        userAttendingEvents = [] #I have no idea what the difference between the first one and this one is but it is requested
+        userAttendingEvents = []  # I have no idea what the difference between the first one and this one is but it is requested
         eventsMaxPop = []
 
-        if 'leave' in request.form:
+        if 'leave' in request.form and request.method == "POST":
             user = session["user"]
-            #REMOVE THE USER FROM THE EVENT IN DATABASE
-        return render_template("manageEvents.html", usersEvents = usersEvents, userEventsDates = userEventsDates,
-                               userEventsTime = userEventsTime, userAttendingEvents = userAttendingEvents,
-                               eventsMaxPop = eventsMaxPop)
+            # REMOVE THE USER FROM THE EVENT IN DATABASE
+        return render_template("manageEvents.html", usersEvents=usersEvents, userEventsDates=userEventsDates,
+                               userEventsTime=userEventsTime, userAttendingEvents=userAttendingEvents,
+                               eventsMaxPop=eventsMaxPop)
 
     else:
-        return  redirect(url_for("login")) #????
+        return redirect(url_for("login"))  # ????
+
 
 @app.route('/search_browseEvents.html')
 @app.route('/search_browseEvents')
 def search_browseEvents():  # put application's code here
-    events = [] #fill the info
+    events = []  # fill the info
     eventDates = []
     eventTimes = []
     eventLocations = []
     eventPrices = []
     eventPopulation = []
     eventMaxPop = []
-    return render_template("search_browseEvents.html", events = events, eventDates = eventDates, eventTimes = eventTimes,
-                           eventLocations = eventLocations, eventPrices= eventPrices, eventPopulation = eventPopulation,
-                           eventMaxPop = eventMaxPop)
+    return render_template("search_browseEvents.html", events=events, eventDates=eventDates, eventTimes=eventTimes,
+                           eventLocations=eventLocations, eventPrices=eventPrices, eventPopulation=eventPopulation,
+                           eventMaxPop=eventMaxPop)
+
 
 @app.route('/create_editEvents.html', methods=["POST", "GET"])
 @app.route('/create_editEvents', methods=["POST", "GET"])
 def create_editEvents():
-    userInfo = [] #list contating users names and their roles to pull from database
+    userInfo = []  # list contating users names and their roles to pull from database
     if request.method == "POST":
         eventTitle = request.form["title"]
         eventAddress = request.form["address"]
@@ -191,7 +193,6 @@ def create_editEvents():
         ################## push to database!!!!!!!
 
     return render_template("create_editEvents.html", userInfo=userInfo)
-
 
 
 if __name__ == '__main__':
